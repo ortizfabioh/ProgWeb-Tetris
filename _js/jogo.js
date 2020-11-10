08e1714e4d76e806a4f3f9d4ad205ba0015b8e35
ortizfabioh <50219525+ortizfabioh@users.noreply.github.com>
2020-11-10 04:42:40 -0300

    Add todas as funções, faltando debug

--- _js/jogo.js
+++ _js/jogo.js
@@ -1,23 +1,26 @@
 const canvas = document.getElementById('tetris');
 const context = canvas.getContext("2d");
 
-// TAMANHO PADRÃO INICIAL (20x10)
-TAMANHOBLOCO = 20;  // Tamanho de cada bloquinho
-canvas.height = 400;
-canvas.width = 200;
-COLUNAS = canvas.height;
-LINHAS = canvas.width;
-window.onload = gerarTab();  // Gerar tabuleiro ao iniciar a página
-
-
 // Variáveis globais para usar no jogo
 let tabuleiro = [];  // Criando o tabuleiro do Tetris
 let pecaAtual;  // Peça sendo controlada atualmente
 let atualX;
 let atualY;
 let derrota;
-let congelado;  // Bool para verificar se a peça está presa no fundo
+let travado;  // Bool para verificar se a peça está presa no fundo
 let tabuleiroInvertido = false;  // true => peças sobem; false => peças descem
+let intervaloTick;
+let intervaloDesenhoPeca;
+
+
+// TAMANHO PADRÃO INICIAL (20x10)
+TAMANHOBLOCO = 20;  // Tamanho de cada bloquinho
+canvas.height = 400;
+canvas.width = 200;
+LINHAS = Math.floor(canvas.height / TAMANHOBLOCO);
+COLUNAS = Math.floor(canvas.width / TAMANHOBLOCO);
+window.onload = gerarTab();  // Gerar tabuleiro ao iniciar a página
+
 
 // Variáveis dos locais de informação
 let tempoPartida = document.getElementById("tempoPartida");
@@ -32,21 +35,22 @@ let dificuldade = document.getElementById("dificuldade");
 ************************************************************** */
 
 function gerarTab() {  // Função que cria o tabuleiro
-    LINHA = Math.floor(COLUNAS / TAMANHOBLOCO);
-    COLUNA = Math.floor(LINHAS / TAMANHOBLOCO);
-
-    // Gerando cada bloquinho
-    for (let l=0; l<LINHA; l++) {
-        tabuleiro[l] = [];
-        for (let c=0; c<COLUNA; c++) {
-            tabuleiro[l][c] = "Black";  // Criando blocos vazios
-        }
-    }
+    criarMatrizTab();
 
     // Adicionando cor e borda aos bloquinhos
-    for (let l=0; l<LINHA; l++) {
-        for (let c=0; c<COLUNA; c++) {
+    for(let l=0; l<LINHAS; l++) {
+        for(let c=0; c<COLUNAS; c++) {
             desenharBloco(c, l, "black", context);
+        }
+    }
+}
+
+function criarMatrizTab() {  // Função que cria a matriz vazia do tabuleiro
+    // Gerando cada bloquinho
+    for(let l=0; l<LINHAS; l++) {
+        tabuleiro[l] = [];
+        for(let c=0; c<COLUNAS; c++) {
+            tabuleiro[l][c] = 0;  // Criando blocos vazios
         }
     }
 }
@@ -54,23 +58,25 @@ function gerarTab() {  // Função que cria o tabuleiro
 function alterarTamanho() {  // Função para alterar o tamanho do tabuleiro
     const redimensao = document.getElementById("redimensao");
 
-    if (TAMANHOBLOCO == 20 && LINHAS == 200 && COLUNAS == 400) {
+    if(TAMANHOBLOCO == 20) {
         TAMANHOBLOCO = 10;
-        LINHAS = 220;
-        COLUNAS = 420;
+        canvas.width = 220;
+        canvas.height = 440;
 
         redimensao.innerHTML = "Mudar para 20 x 10";
-        
-        gerarTab();
+
     } else {
         TAMANHOBLOCO = 20;
-        LINHAS = 200;
-        COLUNAS = 400;
+        canvas.width = 200;
+        canvas.height = 400;
         
         redimensao.innerHTML = "Mudar para 44 x 22";
-
-        gerarTab();
+        
     }
+    LINHAS = Math.floor(canvas.height / TAMANHOBLOCO);
+    COLUNAS = Math.floor(canvas.width / TAMANHOBLOCO);
+
+    gerarTab();
 }
 
 
@@ -79,7 +85,7 @@ function alterarTamanho() {  // Função para alterar o tamanho do tabuleiro
 ************************************************************** */
 
 const Pecas = {  // Informações sobre formato e cor das peças  
-    formato: [
+    formatos: [
         [
             1,1,1,1
         ],
@@ -107,7 +113,7 @@ const Pecas = {  // Informações sobre formato e cor das peças
             1,0,0,0
         ]
     ],
-    cor: [
+    cores: [
         "cyan", "yellow", "orange", "blue", "purple", "green", "red"
     ]
 };
@@ -115,64 +121,63 @@ const Pecas = {  // Informações sobre formato e cor das peças
 function desenharBloco(col, lin, corBloco, context) {  // função pra desenhar cada bloco
     context.fillStyle = corBloco;  // Cor da parte de dentro do bloco
     context.fillRect(col * TAMANHOBLOCO, lin * TAMANHOBLOCO, TAMANHOBLOCO, TAMANHOBLOCO);
-    context.strokeStyle = "#696969"; // Cor das bordas do canvas
+    context.strokeStyle = "#696969"; // Cor das bordas de cada bloco
     context.strokeRect(col * TAMANHOBLOCO, lin * TAMANHOBLOCO, TAMANHOBLOCO, TAMANHOBLOCO);
 }
 
 function desenharPeca() {  // Função q será chamada a cada vez que a peça descer um bloco
-    context.clearRect(0, 0, LINHAS, COLUNAS);  // Apagando o bloco
+    context.clearRect(0, 0, canvas.width, canvas.height);  // Apagando o tabuleiro
 
-    for (let x=0; x<COLUNAS; ++x) {
-        for (let y=0; y<LINHAS; ++y) {
-            if (tabuleiro[y][x]) {
-                desenharBloco(x, y, Pecas.cor[tabuleiro[y][x] - 1], context);
+    for(let x=0; x<COLUNAS; ++x) {
+        for(let y=0; y<LINHAS; ++y) {
+            if(tabuleiro[y][x]) {
+                desenharBloco(x, y, Pecas.cores[tabuleiro[y][x] - 1], context);
             }
         }
     }
 
-    for (let y=0; y<4; ++y) {
-        for (let x=0; x<4; ++x) {
-            if (pecaAtual[y][x]) {
-                desenharBloco(atualX+x, atualY+y, Pecas.cor[pecaAtual[y][x] - 1], context);
+    for(let y=0; y<4; ++y) {
+        for(let x=0; x<4; ++x) {
+            if(pecaAtual[y][x]) {
+                desenharBloco(atualX+x, atualY+y, Pecas.cores[pecaAtual[y][x] - 1], context);
             }
         }
     }
 }
 
 function novaPeca() {  // Escolhe uma peça aleatória e define posição de spawn dela
-    let id = Math.floor(Math.random() * formatoPecas.length);  // Gerar um índice aleatório para pegar um formato de peça
-    let formato = formatoPecas[id]; // O id será usado para pegar a cor da peça
+    let id = Math.floor(Math.random() * Pecas.formatos.length);  // Gerar um índice aleatório para pegar um formato de peça
+    let formato = Pecas.formatos[id];
 
     pecaAtual = [];
-    for (let y=0; y<4; ++y) {
+    for(let y=0; y<4; ++y) {
         pecaAtual[y] = [];
-        for (let x=0; x<4; ++x) {
+        for(let x=0; x<4; ++x) {
             let i = 4 * y + x;
-            if (typeof formato[i] != 'undefined' && formato[i] ) { // Verificar se existe aquele formato
+            if(typeof formato[i] != 'undefined' && formato[i]) { // Verificar se existe aquele formato
                 pecaAtual[y][x] = id + 1;
-            }
-            else {
+            } else {
                 pecaAtual[y][x] = 0;
             }
         }
     }
     
-    congelado = false;
+    travado = false;
     // Local onde as peças irão surgir
     if(TAMANHOBLOCO == 20) {
-        currentX = 5;
-        currentY = 0;
+        atualX = 5;
+        atualY = 0;
     } else {
-        currentX = 11;
-        currentY = 0;
+        atualX = 11;
+        atualY = 0;
     }
 }
 
 function rotacionar(pecaAtual) {  // Rotaciona a peça em sentido anti-horário
     let novoAtual = [];
-    for (let y=0; y<4; ++y) {
+    for(let y=0; y<4; ++y) {
         novoAtual[y] = [];
-        for (let x=0; x<4; ++x) {
+        for(let x=0; x<4; ++x) {
             novoAtual[y][x] = pecaAtual[3-x][y];
         }
     }
@@ -185,37 +190,36 @@ function rotacionar(pecaAtual) {  // Rotaciona a peça em sentido anti-horário
 ********************* MOVIMENTAÇÃO DAS PEÇAS ********************
 ************************************************************** */
 
-getElementById("tetris").addEventListener("keydown", controle);  // Espera uma tecla sem valor (setas) ser pressionada no jogo
+document.addEventListener("keydown", controle);  // Espera uma tecla sem valor (setas) ser pressionada no jogo
 
-// Funcao que move a peça de acordo com o clique
-function controle(event) {
-    if (event.key === "ArrowLeft") {  // Seta esquerda
-        if (movimentoValido(-1)) {
+function controle(event) {  // Função que atribui os movimentos das setas
+    if(event.key === "ArrowLeft") {  // Seta esquerda
+        if(movimentoValido(-1)) {
             --atualX;
         }
-    } else if (event.key === "ArrowRight") {  // Seta direita
-        if (movimentoValido(1)) {
+    } else if(event.key === "ArrowRight") {  // Seta direita
+        if(movimentoValido(1)) {
             ++atualX;
         }
-    } else if (event.key === "ArrowDown") {  // Seta baixo
+    } else if(event.key === "ArrowDown") {  // Seta baixo
         if(!tabuleiroInvertido) {  // Tabuleiro comum
-            while(movimentoValido(0, 1)) {
+            while(movimentoValido(0, 1)) {  // Pra poder mover a peça até o final
                 ++atualY;
-            }        
+            }  
         } else {
             var rotacionado = rotacionar(pecaAtual);
-            if (movimentoValido(0, 0, rotacionado)) {
+            if(movimentoValido(0, 0, rotacionado)) {
                 pecaAtual = rotacionado;
             }
         }
-    } else if (event.key === "ArrowUp") {  // Seta cima
+    } else if(event.key === "ArrowUp") {  // Seta cima
         if(tabuleiroInvertido) {  // Tabuleiro invertido
-            while(movimentoValido(0, -1)) {
+            while(movimentoValido(0, -1)) {  // Pra poder mover a peça até o final
                 --atualY;
             }        
         } else {
             var rotacionado = rotacionar(pecaAtual);
-            if (movimentoValido(0, 0, rotacionado)) {
+            if(movimentoValido(0, 0, rotacionado)) {
                 pecaAtual = rotacionado;
             }
         }
@@ -227,22 +231,22 @@ function controle(event) {
 ************ REGRAS DE JOGO E GERENCIAMENTO DA PARTIDA **********
 ************************************************************** */
 
-function movimentoValido(proxX, proxY, novoAtual ) {
+function movimentoValido(proxX, proxY, novoAtual ) {  // Verifica se a próxima posição da peça é válida
     // Operadores || nas variáveis significa que caso o primeiro valor seja false, o segundo valor será atribuído
     proxX = proxX || 0;
     proxY = proxY || 0;
     proxX = atualX + proxX;
     proxY = atualY + proxY;
-    novoAtual = novoAtual || current;
+    novoAtual = novoAtual || pecaAtual;
 
-    for (let y=0; y<4; ++y) {
-        for (let x=0; x<4; ++x) {
-            if (novoAtual[y][x]) {
-                if (typeof tabuleiro[y+proxY] == 'undefined' || typeof tabuleiro[y+proxY][x+proxX] == 'undefined'
-                  || y+proxY >= LINHAS || x+proxX >= COLUNAS
+    for(let y=0; y<4; ++y) {
+        for(let x=0; x<4; ++x) {
+            if(novoAtual[y][x]) {
+                if(typeof tabuleiro[y+proxY] == 'undefined' || typeof tabuleiro[y+proxY][x+proxX] == 'undefined'
+                  || y+proxY >= canvas.width || x+proxX >= canvas.height
                   || tabuleiro[y+proxY][x+proxX]
                   || x+proxX < 0) {
-                    if (proxY == 1 && freezed) {  // Peça presa na primeira linha
+                    if(proxY == 1 && travado) {  // Peça presa na primeira linha
                         derrota = true; 
                     } 
                     return false;
@@ -253,8 +257,79 @@ function movimentoValido(proxX, proxY, novoAtual ) {
     return true;
 }
 
-function iniciar() {
+function tick() {  // Mantém a peça movendo pra baixo/cima
+    if(!tabuleiroInvertido) {
+        if(movimentoValido(0, 1)) {
+            ++atualY;
+        } else {
+            travarPeca();
+            movimentoValido(0, 1);
+            apagarLinhaPreenchida();
+            if(derrota) {
+                resetIntervalos();
+                return false;
+            }
+            novaPeca();
+        }
+    } else {
+        if(movimentoValido(0, -1)) {
+            --atualY;
+        } else {
+            travarPeca();
+            movimentoValido(0, -1);
+            apagarLinhaPreenchida();
+            if(derrota) {
+                resetIntervalos();
+                return false;
+            }
+            novaPeca();
+        }
+    }
+}
 
+function travarPeca() {  // Trava a peça no tabuleiro ao final do movimento
+    for(let y=0; y<4; ++y) {
+        for(let x=0; x<4; ++x) {
+            if(pecaAtual[y][x]) {
+                tabuleiro[y+atualY][x+atualX] = pecaAtual[y][x];
+            }
+        }
+    }
+    travado = true;
+}
+
+function apagarLinhaPreenchida() {  // Verifica se alguma linha está preenchida e apaga se estiver
+    for(let y=canvas.width-1; y>=0; --y) {
+        let linhaPreenchida = true;
+        for(let x=0; x<canvas.height; ++x) {
+            if (tabuleiro[y][x] == 0) {  // Bloco vazio
+                linhaPreenchida = false;
+                break;
+            }
+        }
+        if(linhaPreenchida) {
+            for(let y2=y; y2>0; --y2) {
+                for(let x=0; x<canvas.height; ++x) {
+                    tabuleiro[y2][x] = tabuleiro[y2-1][x];
+                }
+            }
+            ++y;
+        }
+    }
+}
+
+function resetIntervalos(){  // Reseta os intervalos criados no jogo
+    clearInterval(intervaloTick);
+    clearInterval(intervaloDesenhoPeca);
+}
+
+function iniciar() {
+    resetIntervalos();
+    intervaloDesenhoPeca = setInterval(desenharPeca, 3);
+    criarMatrizTab();
+    novaPeca();
+    derrota = false;
+    intervaloTick = setInterval(tick, 400);
 }
 
 function pausar() {
